6b15b85b9d1ee10440acb1d7ff8ccbda83bf1d93
Mrochen, David <david.mrochen@mediservbank.de>
2024-05-24 21:11:56 +0200

    Draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +12,15 @@ export class AppComponent {
 
   btnNextClicked(){
     const folder = '../assets/world-maps';
-    
-    
+    fs.readdir(folder, (err, files) => {
+      if(err){
+        return console.log('Fehler beim Lesen des Ordners: ' + err);
+      }
+
+      files.forEach(file => {
+        console.log(file);
+      });
+    });
   }
 
   btnPrevClicked(){
